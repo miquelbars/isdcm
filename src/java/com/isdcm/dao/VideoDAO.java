5a7ea4c76d71ff52155c3dbab6d719bd6eb69d54
fiblabs <fiblabs@DESKTOP-30J8P41>
2021-03-17 00:30:52 +0100

    Modification and optimization in VideoDAO

--- src/java/com/isdcm/dao/VideoDAO.java
+++ src/java/com/isdcm/dao/VideoDAO.java
@@ -3,6 +3,8 @@ package com.isdcm.dao;
 import com.isdcm.dto.VideoDTO;
 import com.isdcm.jdbc.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VideoDAO {
     
@@ -11,17 +13,10 @@ public class VideoDAO {
         try {
             DataSource ds = DataSource.getInstance();
             //here mydb is database name, mydb is username and password
-//            Statement stmt = ds.getConnection().createStatement();
-//            ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM MYDB.VIDEO");
-//            if(rs.next()) {
-//                
-//            } 
             String insertNewUserSQL = "INSERT INTO MYDB.VIDEO (TITULO, AUTOR, FECHA_CREACION, DURACION, REPRODUCCIONES, DESCRIPCION, FORMATO) VALUES (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement pstmt = ds.getConnection().prepareStatement(insertNewUserSQL);
-//            pstmt.setInt(1, rs.getInt(1));
             pstmt.setString(1, video.getTitulo());
             pstmt.setString(2, video.getAutor());
-            //Modificar-ho més endavant
             java.sql.Date date1 = new java.sql.Date(video.getFechaCreacion().toEpochDay());
             java.sql.Time time1 = new java.sql.Time(video.getDuracion().toNanoOfDay());
             pstmt.setDate(3, date1);
@@ -33,5 +28,31 @@ public class VideoDAO {
             ds.getConnection().close();  
         }
         catch(SQLException e){ System.out.println(e);}
+    }
+    
+    public List getVideos(){
+        List<VideoDTO> L = new ArrayList<VideoDTO>();
+        try{
+            DataSource ds = DataSource.getInstance();
+            String sql = "SELECT * FROM MYDB.VIDEO";
+            PreparedStatement statement = ds.getConnection().prepareStatement(sql);
+
+            ResultSet result = statement.executeQuery();
+
+            while(result.next()){
+                VideoDTO video = new VideoDTO();
+                video.setTitulo(result.getString("titulo"));
+                video.setAutor(result.getString("autor"));
+                video.setFechaCreacion(result.getDate("fecha_creacion").toLocalDate());
+                video.setDuracion(result.getTime("duracion").toLocalTime());
+                video.setReproducciones(result.getInt("reproducciones"));
+                video.setDescripcion(result.getString("descripcion"));
+                video.setFormato(result.getString("formato"));
+                L.add(video);
+            }
+            ds.getConnection().close();
+        }
+        catch(SQLException e){ System.out.println(e);}
+        return L;
     }
 }
